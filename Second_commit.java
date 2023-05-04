94bf91defe12f168a8645f8a33fc0fa65abdcf26
eujin-shin <eujinshin0124@gmail.com>
2023-05-04 18:43:45 +0900

    ej commit

--- Second_commit.java
+++ Second_commit.java
@@ -2,7 +2,7 @@ public class Second_commit {
 
     public static void main(String[] args) {
         System.out.println("second_Commit");
-        System.out.println("sh commit");
+        System.out.println("ej commit");
     }
 
 }
